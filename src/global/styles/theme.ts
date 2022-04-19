07ba3104c897662aa7a3454ffcf1e6daf7b76de0
Vitor Paiva <paiva.v@icloud.com>
2022-04-19 18:56:51 -0300

    style :nail_care:(global style): added app theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,33 @@
+export default {
+  colors: {
+    primary: '#29166F',
+    secondary: '#DA251C',
+
+    secondary100: '#0A1033',
+    secondary90: '#0D133D',
+    secondary85: '#171F52',
+    secondary80: '#0E1647',
+    secondary70: '#1B2565',
+    secondary60: '#1B2565',
+    secondary50: '#243189',
+    secondary40: '#1D2766',
+    secondary30: '#495BCC',
+
+    highlight: '#9387C0',
+    highlight_dark: '#9387C0',
+
+    overlay: 'rgba(0, 0, 0, 0.7)',
+
+    input_text: '#D5DDE0',
+    input_text_placeholder: '#8F8F8F',
+    text_light: '#FFFFFF',
+    text: '#8F8F8F',
+    text_dark: '#353839',
+
+    shape: '#FFFFFF',
+
+  },
+  fonts: {
+
+  }
+}
