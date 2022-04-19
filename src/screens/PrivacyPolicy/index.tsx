6feeb09e3e8aeb9772baba2ff6c28ad6c50d8ebd
Vitor Paiva <paiva.v@icloud.com>
2022-04-19 19:06:33 -0300

    feat :sparkles:(terms of use): added terms of use and privacy policy screens

---
+++ src/screens/PrivacyPolicy/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  Container,
+  ScrollView,
+  UpdateDate,
+  Title,
+  SubTitle,
+  SubSubTitle,
+  Text
+} from './styles';
+
+import Unorderedlist from 'react-native-unordered-list';
+
+export function PrivacyPolicy() {
+  return (
+    <Container>
+      <ScrollView>
+        <UpdateDate>Última alteração: 19/04/2022</UpdateDate>
+        <Title>Aviso de Privacidade da Fast Taxi</Title>
+        <SubTitle>I. Introdução</SubTitle>
+        <Text>
+          Ao usar os apps da Fast Taxi, você nos confia seus dados pessoais. Temos o compromisso de manter essa confiança. Para começar, queremos ajudar você a entender as nossas práticas de privacidade.{'\n'}
+          {'\n'}
+          Este aviso descreve os dados pessoais que coletamos, como são usados e compartilhados e as suas opções com relação a eles.
+        </Text>
+        <SubTitle>II. Visão geral</SubTitle>
+        <SubSubTitle>A. Escopo</SubSubTitle>
+        <Text>
+          Este aviso descreve de que maneira a Uber e suas afiliadas coletam e usam dados pessoais e é aplicável a todos os usuários dos nossos apps, sites, recursos ou outros serviços em qualquer lugar do mundo. Esse aviso se aplica especificamente a:
+          
+        </Text>
+      </ScrollView>
+    </Container>
+  );
+}
---
+++ src/screens/TermsOfUse/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components/native';
+
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 5%;
+`;
+export const ScrollView = styled.ScrollView.attrs({
+  contentContainerStyle: {
+  }
+})`
+  margin-top: ${getStatusBarHeight() + RFValue(30)}px;
+`;
+export const UpdateDate = styled.Text`
+  text-align: center;
+`;
+export const Title = styled.Text`
+  text-align: center;
+  font-size: ${RFValue(28)}px;
+  font-weight: bold;
+`;
+export const SubTitle = styled.Text`
+  font-size: ${RFValue(16)}px;
+  font-weight: bold;
+`;
+export const Text = styled.Text`
+`;
