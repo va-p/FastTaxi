3792231fd8a210ccc0b67212585b2f444433bb03
Vitor Paiva <paiva.v@icloud.com>
2022-04-19 19:28:43 -0300

    feat :sparkles:(support and reset pass): added support and reset pass screens

---
+++ src/screens/Support/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  View
+} from 'react-native';
+
+import { styles } from './styles';
+
+export function Support() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
